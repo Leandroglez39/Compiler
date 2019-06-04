14743f590a28e4b5acf06263faec12589cb401a3
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 16:10:09 -0400

    Dependencias en Program.cs

--- CoolCompiler/Program.cs
+++ CoolCompiler/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+using CoolCompiler.AST;
+using Antlr4.Runtime.Tree;
 
 namespace CoolCompiler
 {
