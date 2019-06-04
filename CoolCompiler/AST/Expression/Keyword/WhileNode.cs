6726aef1254466b4ad880a51a9250350079cf15b
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:04:58 -0400

    WhileNode

---
+++ CoolCompiler/AST/Expression/Keyword/WhileNode.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Keyword
+{
+    public class WhileNode :KeywordNode
+    {
+        public ExpressionNode Condition { get; set; }
+        public ExpressionNode Body { get; set; }
+
+        public WhileNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
