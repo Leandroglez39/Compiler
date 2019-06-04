462ae639a61c50979fb2663670b209f43a3c8b23
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:53:36 -0400

    MulNode

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Arithmetic/MulNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Operators.Binary.Arithmetic
+{
+    public class MulNode : ArithmeticOperation
+    {
+        public override string Symbol => "*";
+
+        public MulNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
