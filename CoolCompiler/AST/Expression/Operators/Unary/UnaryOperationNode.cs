cc2cf79dba32aa9362ad9d612b5b2d6246180c92
Unknown <unknown@example.com>
2019-06-04 13:31:02 -0400

    UnaryOperators

---
+++ CoolCompiler/AST/Expression/Operators/Unary/UnaryOperationNode.cs
@@ -0,0 +1,15 @@
+﻿using Antlr4.Runtime;
+
+namespace CoolCompiler.AST.Expression.Operators.Unary
+{
+    public abstract class UnaryOperationNode : ExpressionNode
+    {
+        public ExpressionNode Operand { get; set; }
+
+        public abstract string Symbol { get; }
+
+        public UnaryOperationNode(ParserRuleContext context) : base(context)
+        {
+        }
+    }
+}
