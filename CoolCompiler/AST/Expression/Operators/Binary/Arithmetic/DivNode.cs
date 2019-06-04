08785ae9c3976dad52b19a6ad50129931b95ae02
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:51:58 -0400

    DivNode

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Arithmetic/DivNode.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Operators.Binary.Arithmetic
+{
+    public class DivNode : ArithmeticOperation
+    {
+        public override string Symbol => "/";
+
+        public DivNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
