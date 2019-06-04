fe800e25f803736096e4c98ec1c85a639244dda7
Unknown <unknown@example.com>
2019-06-04 13:26:44 -0400

    AssignmentNode

---
+++ CoolCompiler/AST/Expression/AssignmentNode.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression
+{
+    public class AssignmentNode :ExpressionNode
+    {
+
+        public IdentifierNode ID { get; set; }
+        public ExpressionNode ExpressionRight { get; set; }
+
+        public AssignmentNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
