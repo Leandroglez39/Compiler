53a75bf493183cd59056195e1581f73bbf2e5580
Unknown <unknown@example.com>
2019-06-04 13:37:32 -0400

    IsVoidNode

---
+++ CoolCompiler/AST/Expression/Operators/Unary/IsVoidNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Operators.Unary
+{
+    public class IsVoidNode : UnaryOperationNode
+    {
+        public override string Symbol => "isvoid";
+
+        public IsVoidNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
