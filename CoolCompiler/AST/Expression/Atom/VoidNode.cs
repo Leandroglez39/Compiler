76f99f5a58765acf7316fcfe260400cb75d1c2be
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:32:34 -0400

    VoidNode

---
+++ CoolCompiler/AST/Expression/Atom/VoidNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Atom
+{
+    public class VoidNode : AtomNode
+    {
+        public string GetStaticType { get; }
+        public VoidNode(string type, int line = 0, int column = 0) : base(line, column)
+        {
+            GetStaticType = type;
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
