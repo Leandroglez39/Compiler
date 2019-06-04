dad377073f03fad2fe6a2b558fa2ca7f88790c5a
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:03:31 -0400

    IfNode

---
+++ CoolCompiler/AST/Expression/Keyword/IfNode.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+
+namespace CoolCompiler.AST.Expression.Keyword
+{
+    public class IfNode : KeywordNode
+    {
+        public ExpressionNode Condition { get; set; }
+        public ExpressionNode Body { get; set; }
+        public ExpressionNode ElseBody { get; set; }
+
+        public IfNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
