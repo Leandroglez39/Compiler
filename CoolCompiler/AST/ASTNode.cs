1bd2ee8ff6e6030f91c16a47760f6f481771f040
Unknown <unknown@example.com>
2019-06-04 11:30:05 -0400

    Creado ASTNode

---
+++ CoolCompiler/AST/ASTNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+namespace CoolCompiler.AST
+{
+    public abstract class ASTNode
+    {
+        public int Line { get; }
+
+        public int Column { get; }
+
+        public Dictionary<string, dynamic> Attributes { get; }
+
+        public ASTNode(ParserRuleContext context)
+        {
+            Line = context.Start.Line;
+            Column = context.Start.Column + 1;
+            Attributes = new Dictionary<string, dynamic>();
+        }
+
+        public ASTNode(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+    }
+}
