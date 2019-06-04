d8f7d8edb2956bed2597f66ec77763bf1cf5aefa
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 16:01:00 -0400

    LessEqual

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Comparison/LessEqual.cs
@@ -0,0 +1,20 @@
+﻿using CoolCompiler.SemanticCheck;
+using Antlr4.Runtime;
+
+namespace CoolCompiler.AST
+{
+    
+        public class LessEqual : ComparisonOperation
+        {
+            public override string Symbol => "<=";
+
+            public LessEqual(ParserRuleContext context) : base(context)
+            {
+            }
+
+            public override void Accept(IVisitor visitor)
+            {
+                visitor.Visit(this);
+            }
+        }
+}
