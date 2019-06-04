54271b7b1ad9e213790e6050afd170a8183e6f04
Unknown <unknown@example.com>
2019-06-04 13:08:18 -0400

    FormalNode

---
+++ CoolCompiler/AST/Auxiliary/FormalNode.cs
@@ -0,0 +1,21 @@
+﻿using CoolCompiler.AST.Expression;
+using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST
+{
+    public class FormalNode : ExpressionNode
+    {
+        public IdentifierNode Id { get; set; }
+        public TypeNode Type { get; set; }
+
+        public FormalNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
