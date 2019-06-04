223d967fcb64ba960c362e2ea9bc948c3d5d76a9
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:10:35 -0400

    Dispatch

---
+++ CoolCompiler/AST/Expression/DispatchNode.cs
@@ -0,0 +1,18 @@
+﻿
+using Antlr4.Runtime;
+using System.Collections.Generic;
+
+namespace CoolCompiler.AST.Expression
+{
+    public abstract class DispatchNode : ExpressionNode
+    {
+        public IdNode IdMethod { get; set; }
+
+        public List<ExpressionNode> Arguments { get; set; }
+
+        public DispatchNode(ParserRuleContext context) : base(context)
+        {
+
+        }
+    }
+}
