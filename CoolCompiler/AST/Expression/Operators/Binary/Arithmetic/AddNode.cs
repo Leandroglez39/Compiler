97d9d3ac7c692cae7b15f39796321812116de515
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:49:08 -0400

    AddNode

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Arithmetic/AddNode.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Operators.Binary.Arithmetic
+{
+    public class AddNode : ArithmeticOperation
+    {
+        public override string Symbol => "+";
+
+        public AddNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
