37124b9064e0e0aff84f240154aaf957ffdc1721
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:33:58 -0400

    NegNode

---
+++ CoolCompiler/AST/Expression/Operators/Unary/NegNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Operators.Unary
+{
+    public class NegNode : UnaryOperationNode
+    {
+        public override string Symbol => "~";
+
+        public NegNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
