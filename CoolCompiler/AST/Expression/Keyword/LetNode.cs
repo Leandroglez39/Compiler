359002bc8e89e03865831b4a95496e8fa2de1f7f
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:06:39 -0400

    LetNode

---
+++ CoolCompiler/AST/Expression/Keyword/LetNode.cs
@@ -0,0 +1,23 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+using System.Collections.Generic;
+
+
+
+namespace CoolCompiler.AST.Expression.Keyword
+{
+    public class LetNode : KeywordNode
+    {
+        public List<AttributeNode> Initialization { get; set; }
+        public ExpressionNode ExpressionBody { get; set; }
+
+        public LetNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
