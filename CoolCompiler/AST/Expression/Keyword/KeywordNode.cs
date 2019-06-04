9a1bf83bad03f68af59a1fee5cf6518dc0250d81
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:58:38 -0400

    KeywordNode

---
+++ CoolCompiler/AST/Expression/Keyword/KeywordNode.cs
@@ -0,0 +1,11 @@
+﻿using Antlr4.Runtime;
+
+namespace CoolCompiler.AST.Expression.Keyword
+{
+    public abstract class KeywordNode : ExpressionNode
+    {
+        public KeywordNode(ParserRuleContext context) : base(context)
+        {
+        }
+    }
+}
