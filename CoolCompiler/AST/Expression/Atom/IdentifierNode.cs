c81957372d720bb599abaa254a7c021d6d0deb62
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:22:56 -0400

    IdentifierNode

---
+++ CoolCompiler/AST/Expression/Atom/IdentifierNode.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression
+{
+    public class IdentifierNode :AtomNode
+    {
+        public string Text { get; set; }
+
+        public IdentifierNode(ParserRuleContext context, string text) : base(context)
+        {
+            Text = text;
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
