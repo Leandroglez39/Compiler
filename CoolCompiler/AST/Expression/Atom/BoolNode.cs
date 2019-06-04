a1ed1ed33473350d626bf4eafae61f21153d6dc3
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:21:47 -0400

    BoolNode

---
+++ CoolCompiler/AST/Expression/Atom/BoolNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression
+{
+    public class BoolNode :AtomNode
+    {
+        public bool Value { get; set; }
+
+        public BoolNode(ParserRuleContext context, string text) : base(context)
+        {
+            Value = bool.Parse(text);
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
