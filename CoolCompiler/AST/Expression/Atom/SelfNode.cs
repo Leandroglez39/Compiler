bfe51a2607b156d4d5fb980969452ed2b32eebb9
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:28:03 -0400

    SelfNode

---
+++ CoolCompiler/AST/Expression/Atom/SelfNode.cs
@@ -0,0 +1,17 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Atom
+{
+    class SelfNode :AtomNode
+    {
+        public SelfNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
