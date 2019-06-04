e9e9d82b77ab5854cbc440d61ebaa96b60d1f533
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:16:27 -0400

    DispatchNodes

---
+++ CoolCompiler/AST/Expression/Dispatch/DispatchExplicitNode.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression
+{
+    public class DispatchExplicitNode : DispatchNode
+    {
+        public ExpressionNode Expression { get; set; }
+        public TypeNode IdType { get; set; }
+
+        public DispatchExplicitNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
---
+++ CoolCompiler/AST/Expression/Dispatch/DispatchImplicitNode.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+
+namespace CoolCompiler.AST.Expression
+{
+    public class DispatchImplicitNode : DispatchNode
+    {
+        public DispatchImplicitNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
