229477efb234912b8e92c582001d9446c1c3e940
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:38:31 -0400

    SequenceNode

---
+++ CoolCompiler/AST/Expression/Secuence/SequenceNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+using System.Collections.Generic;
+
+namespace CoolCompiler.AST.Expression.Secuence
+{
+    public class SequenceNode :ExpressionNode
+    {
+        public List<ExpressionNode> Sequence { get; set; }
+
+        public SequenceNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
