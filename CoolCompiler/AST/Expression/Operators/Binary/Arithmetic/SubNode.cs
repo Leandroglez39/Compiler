5469f2c6abe9997459dbd599cc033d2f6b1e34a1
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:54:40 -0400

    SubNode

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Arithmetic/SubNode.cs
@@ -0,0 +1,19 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Operators.Binary.Arithmetic
+{
+    public class SubNode : ArithmeticOperation
+    {
+        public override string Symbol => "/";
+
+        public SubNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
