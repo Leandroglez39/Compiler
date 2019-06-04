9803587d1cb0062961ee8c03f135e5f275e8852f
Unknown <unknown@example.com>
2019-06-04 13:12:56 -0400

    ProgramNode

---
+++ CoolCompiler/AST/ProgramNode.cs
@@ -0,0 +1,22 @@
+﻿using CoolCompiler.SemanticCheck;
+using Antlr4.Runtime;
+using System.Collections.Generic;
+
+
+
+namespace CoolCompiler.AST
+{
+    public abstract class ProgramNode : ASTNode, IVisit
+    {
+        public List<ClassNode> Classes { get; set; }
+
+        public ProgramNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
