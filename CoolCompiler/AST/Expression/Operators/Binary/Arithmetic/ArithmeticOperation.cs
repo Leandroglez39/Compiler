a6bbbf5231fddf27708a2643a1e440b844cec2e2
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:47:40 -0400

    ArithmeticOperation

---
+++ CoolCompiler/AST/Expression/Operators/Binary/Arithmetic/ArithmeticOperation.cs
@@ -0,0 +1,12 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Operators.Binary.Arithmetic
+{
+    public abstract class ArithmeticOperation :BinaryOperationNode
+    {
+        public ArithmeticOperation(ParserRuleContext context) : base(context)
+        {
+        }
+    }
+}
