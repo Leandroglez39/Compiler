139ae5cab6260d0ffbe59b3cf86c4819f11616cd
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 14:43:48 -0400

    BinaryOperator

---
+++ CoolCompiler/AST/Expression/Operators/Binary/BinaryOperationNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+namespace CoolCompiler.AST.Expression.Operators.Binary
+{
+    public abstract class BinaryOperationNode :ExpressionNode
+    {
+        public ExpressionNode LeftOperand { get; set; }
+        public ExpressionNode RightOperand { get; set; }
+
+        public BinaryOperationNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public abstract string Symbol { get; }
+    }
+}
