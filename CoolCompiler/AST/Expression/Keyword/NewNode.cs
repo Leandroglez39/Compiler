2e433a4f3ce53535481e04204ee48c00defd4435
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:07:46 -0400

    NewNode

---
+++ CoolCompiler/AST/Expression/Keyword/NewNode.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression.Keyword
+{
+    public class NewNode : KeywordNode
+    {
+        public TypeNode TypeId { get; set; }
+
+        public NewNode(ParserRuleContext context) : base(context)
+        {
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
