2e59872ac18f4362b4294db58f9ea7fe81c138b1
Unknown <unknown@example.com>
2019-06-04 11:40:18 -0400

    Interfaces Visit

---
+++ CoolCompiler/SemanticCheck/IVisit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCompiler.SemanticCheck
+{
+    public interface IVisit
+    {
+        void Accept(IVisitor visitor);
+    }
+}
---
+++ CoolCompiler/SemanticCheck/IVisitor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCompiler.SemanticCheck
+{
+    public interface IVisitor
+    {
+        void Visit(ArithmeticOperation node);
+        void Visit(AssignmentNode node);
+        void Visit(AttributeNode node);
+        void Visit(BoolNode node);
+        void Visit(CaseNode node);
+        void Visit(ClassNode node);
+        void Visit(ComparisonOperation node);
+        void Visit(DispatchExplicitNode node);
+        void Visit(DispatchImplicitNode node);
+        void Visit(EqualNode node);
+        void Visit(FormalNode node);
+        void Visit(IdentifierNode node);
+        void Visit(IfNode node);
+        void Visit(IntNode node);
+        void Visit(IsVoidNode node);
+        void Visit(LetNode node);
+        void Visit(MethodNode node);
+        void Visit(NegNode node);
+        void Visit(NewNode node);
+        void Visit(NotNode node);
+        void Visit(ProgramNode node);
+        void Visit(SelfNode node);
+        void Visit(SequenceNode node);
+        void Visit(StringNode node);
+        void Visit(VoidNode node);
+        void Visit(WhileNode node);
+
+    }
+}
