6c57b8f545f766d5d348d9e36e2c0e8ab717af75
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-04 15:25:16 -0400

    IntNode

---
+++ CoolCompiler/AST/Expression/Atom/IntNode.cs
@@ -0,0 +1,21 @@
+﻿using Antlr4.Runtime;
+using CoolCompiler.SemanticCheck;
+
+
+namespace CoolCompiler.AST.Expression
+{
+    public class IntNode :AtomNode
+    {
+        public int Value { get; set; }
+
+        public IntNode(ParserRuleContext context, string text) : base(context)
+        {
+            Value = int.Parse(text);
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
