a19ecfbc73e565543d081335ba6f626be3fadc20
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2019-06-15 15:20:13 -0400

    Param

---
+++ CoolCompiler/CodeGenerations/3AC/Param.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolCompiler.CodeGenerations
+{
+    public class Param : ThreeCode
+    {
+        public int VariableCounter;
+        public Param(int variable_counter)
+        {
+            VariableCounter = variable_counter;
+        }
+
+        public override void Accept(ICodeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public override string ToString()
+        {
+            return "PARAM t" + VariableCounter + ";";
+        }
+    }
+}
